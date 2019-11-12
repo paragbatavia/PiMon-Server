a69bc63c9620a7f80dc012938e09660c3e999812
Parag Batavia <parag@bataviafamily.com>
2019-11-12 10:06:45 -0500

    Added skeleton for resetting router, and flag for printing subprocess (ping) output

--- PiMonSrv.py
+++ PiMonSrv.py
@@ -1,8 +1,13 @@
 import os
 import platform
 import subprocess
+import time
+import sys
 
-def ping(host):
+host = str("8.8.8.8")
+
+
+def ping(host, printOutput):
     """
     Returns True if host (str) responds to a ping request
     Code taken from: https://stackoverflow.com/questions/2953462/pinging-servers-in-python
@@ -14,11 +19,37 @@ def ping(host):
     # Build the command
     command = ['ping', param, '1', host]
 
-    return subprocess.call(command) == 0
+    if not printOutput:
+        FNULL = subprocess.DEVNULL
+    else:
+        FNULL = sys.stdout # only this works - subprocess.STDOUT fails
+        
+    return subprocess.call(command, stdout=FNULL, stderr=FNULL) == 0
 
 
-host = str("8.8.8.8")
+def resetRouter(downTime, waitTime):
+    """
+    Does a simple sequence to reset the router
+    downTime: how long to power down the router (float, seconds)
+    waitTime: how long to wait before testing the router for activity
+    """
 
-retVal = ping(host)
-print ("returned " + str(retVal))
+    # send a power down command
+
+    time.sleep(downTime)
+
+    # send a power up command
+
+    # wait for everything to come back up
+    time.sleep(waitTime)
+
+    # did it work?
+    return ping(host)
+    
+
+
+while 1:
+    retVal = ping(host, True)
+    print ("returned " + str(retVal))
+    time.sleep(2.0)
 
